053df8df91d94b838efcb7958e47abcd44c2091c
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-16 03:04:46 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,23 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which === "g") {
+      alert('g was pressed');
+    }
+  })
+}
